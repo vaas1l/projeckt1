a64c9d1784edb1cd7c139dfb5388c2806f4ae763
Maryntsivskyy Vasyl <vasamar0356@gmail.com>
2025-01-18 16:16:35 +0100

    ukol3-hotovo

--- ukol3/app.js
+++ ukol3/app.js
@@ -10,15 +10,25 @@ let id = 1
 
 const todos = [
   {
-    id: id++,
+    id: 1,
     text: 'Vzít si dovolenou',
     done: false,
   },
   {
-    id: id++,
+    id: 2,
     text: 'Koupit Elden Ring',
     done: false,
   },
+  {
+    id: 3,
+    text: 'Naučit se JavaScript',
+    done: false,
+  },
+  {
+    id: 5,
+    text: 'Neco udelat',
+    done: false,
+  }
 ]
 
 app.get('/', (req, res) => {
@@ -27,6 +37,13 @@ app.get('/', (req, res) => {
 		todos,
 	})
 })
+
+app.post('/toggle/:id', (req, res) => {
+  const todo = todos.find(t => t.id == req.params.id);
+  if (todo) todo.done = !todo.done;
+  res.redirect('/');
+});
+
 
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`)
